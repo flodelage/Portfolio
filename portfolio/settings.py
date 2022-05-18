152ee75b8db094cd6854003abe5a26596dd10ca5
Florian Delage <delageflo@ymail.com>
2022-05-18 20:50:00 +0200

    add favicon

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.getenv('DJANGO_KEY')
 DEBUG = False if os.environ.get('ENV', 'development') == 'production' else True
 
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.herokuapps.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
